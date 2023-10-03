f4970bb45f5dec9a0c98a45a0a9492eda8011777
Christian Thorjussen <christianbern@gmail.com>
2023-10-03 16:28:59 +0200

    finish s-learner

--- Estimation total and direct effect S-learner.R
+++ Estimation total and direct effect S-learner.R
@@ -182,23 +182,23 @@ treat3$feed <- "Feed 3"
 colnames(treat3) <- c('values', 'feed')
 df <- rbind(treat1, treat2, treat3)
 
-ggplot(data = df, aes(x = feed, y = values)) +
+plot_total <- ggplot(data = df, aes(x = feed, y = values)) +
   geom_boxplot() +
   xlab("Growth Feed Type") +
   ylab("Treatment effect on the prevalence (1/1000) ascites") +
-  ggtitle("S-learner ATE Total") 
+  ggtitle("S-learner ATE Total")
 
+png("C:/broiler_acites/ascites_case/Results/plot_total.png", width = 800, height = 600)  # Adjust width and height as needed
+plot(plot_total, col = "lightblue", main = " ", xlab = "Difference in R-Squared", freq = F)
+dev.off()
 #-----------------------------------------------------------------------------
 # Direct effect where W union X is Birds p m-sqr, Chicken type, Food consumption, Growth, Growth$^s$, Indoor Temperature, Month, Outdoor temperature, Slaughterhouse, Water consumption, start weight
 #-----------------------------------------------------------------------------
 
-# Treatment feed 1
-levels = 4  # Set the number of levels other than other
-data$feed_group = fct_lump_n(data$feed_name, n = levels, other_level = "other")
 # table(data$feed_group)
 data$treatment <-  ifelse(data$feed_group == "Kromat Kylling 2 Enkel u/k", 1, 0)
 
-formula <- ascites_prev ~ treatment + prod_type + frequent_month + id_slaughterhouse
+formula <- ascites_prev ~ treatment + prod_type + average_food + growth + sqr_growth + indoor_mean_maxtemp + frequent_month + climate_mean_temp + id_slaughterhouse + average_water + start_weight
 
 label <- subset(data, select = c(as.character(update(formula, . ~ .)[[2]])))
 
@@ -227,7 +227,139 @@ cv_result <- xgb.cv(
   data = xgb.DMatrix(data = as.matrix(features), label = as.matrix(label)),
   nfold = 10,                     # Number of folds
   verbose = TRUE,
-  nrounds = 30
+  nrounds = 12
+  
+)
+cv <- data.frame(Boosting_Round =  cv_result$evaluation_log$iter, 
+                 Train_Error = cv_result$evaluation_log$train_rmse_mean, 
+                 Test_Error = cv_result$evaluation_log$test_rmse_mean)
+
+train_error <- cv_result$evaluation_log$train_rmse_mean
+test_error <- cv_result$evaluation_log$test_rmse_mean
+
+ggplot(cv, aes(x = Boosting_Round)) +
+  geom_line(aes(y = Train_Error, color = "Train Error"), size = 1) +
+  geom_line(aes(y = Test_Error, color = "Test Error"), size = 1) +
+  scale_color_manual(values = c("Train Error" = "blue", "Test Error" = "red")) +
+  labs(x = "Boosting Round", y = "Error Rate") +
+  theme_minimal() +
+  scale_x_continuous(breaks = seq(0, max(cv$Boosting_Round), by = 10))  #
+
+N <- nrow(data) # Bootstrap obs per resampling
+R = 1000 #Number of bootstrap replica
+
+# Creating a matrix of dirichlet weights for Bayesian bootstrap
+dirichlet <- matrix( rexp(N * R, 1) , ncol = N, byrow = TRUE) 
+dirichlet_w <- dirichlet / rowSums(dirichlet)
+
+S_learner_direct <- boot(data=data, 
+                        statistic = S_learner, 
+                        formula = formula, 
+                        weights = dirichlet_w, 
+                        R=rep(1,R),
+                        eta = 0.1,                      
+                        max_depth = 3,
+                        nrounds = 12)
+
+S_direct_boot_feed1 <- S_learner_direct$t
+save(S_direct_boot_feed1,file="C:/broiler_acites/ascites_case/Results/S_direct_boot_feed1.Rda")
+
+data$treatment <-  ifelse(data$feed_group == "Toppkylling Netto", 1, 0)
+N <- nrow(data) # Bootstrap obs per resampling
+R = 1000 #Number of bootstrap replica
+
+# Creating a matrix of dirichlet weights for Bayesian bootstrap
+dirichlet <- matrix( rexp(N * R, 1) , ncol = N, byrow = TRUE) 
+dirichlet_w <- dirichlet / rowSums(dirichlet)
+
+S_learner_direct <- boot(data=data, 
+                         statistic = S_learner, 
+                         formula = formula, 
+                         weights = dirichlet_w, 
+                         R=rep(1,R),
+                         eta = 0.1,                      
+                         max_depth = 3,
+                         nrounds = 12)
+
+S_direct_boot_feed2 <- S_learner_direct$t
+save(S_direct_boot_feed2,file="C:/broiler_acites/ascites_case/Results/S_direct_boot_feed2.Rda")
+
+
+data$treatment <-  ifelse(data$feed_group == "Kromat Kylling 2 Leg u/k", 1, 0)
+N <- nrow(data) # Bootstrap obs per resampling
+R = 1000 #Number of bootstrap replica
+
+# Creating a matrix of dirichlet weights for Bayesian bootstrap
+dirichlet <- matrix( rexp(N * R, 1) , ncol = N, byrow = TRUE) 
+dirichlet_w <- dirichlet / rowSums(dirichlet)
+
+S_learner_direct <- boot(data=data, 
+                         statistic = S_learner, 
+                         formula = formula, 
+                         weights = dirichlet_w, 
+                         R=rep(1,R),
+                         eta = 0.1,                      
+                         max_depth = 3,
+                         nrounds = 12)
+
+S_direct_boot_feed3 <- S_learner_direct$t
+save(S_direct_boot_feed3,file="C:/broiler_acites/ascites_case/Results/S_direct_boot_feed3.Rda")
+
+treat_d1 <- data.frame(S_direct_boot_feed1[,1])
+treat_d2 <- data.frame(S_direct_boot_feed2[,1])
+treat_d3 <- data.frame(S_direct_boot_feed3[,1])
+treat_d1$feed <- "Feed 1"
+colnames(treat_d1) <- c('values', 'feed')
+treat_d2$feed <- "Feed 2"
+colnames(treat_d2) <- c('values', 'feed')
+treat_d3$feed <- "Feed 3"
+colnames(treat_d3) <- c('values', 'feed')
+df <- rbind(treat_d1, treat_d2, treat_d3)
+
+ggplot(data = df, aes(x = feed, y = values)) +
+  geom_boxplot() +
+  xlab("Growth Feed Type") +
+  ylab("Treatment effect on the prevalence (1/1000) ascites") +
+  ggtitle("S-learner ATE Direct") 
+
+
+#-------------------------------------------------------------------------------------------------
+# Direct effect (attempting CATE) where W union X is Birds p m sqr, Chicken type, Food consumption, Growth_linear, Growth sqr, Indoor Temperature, Indoor humidity, Kg per m sqr, Month, Outdoor humidity, Outdoor temperature, Slaughter age, Slaughterhouse, Water consumption, start weight
+#-----------------------------------------------------------------------------------------------
+
+# table(data$feed_group)
+data$treatment <-  ifelse(data$feed_group == "Kromat Kylling 2 Enkel u/k", 1, 0)
+
+formula <- ascites_prev ~ treatment + birds_m_sqr + prod_type + average_food + growth + sqr_growth + indoor_mean_maxtemp + indoor_mean_maxhumidity + kg_m_sqr + frequent_month + climate_mean_hum + climate_mean_temp + id_slaughterhouse + average_water + start_weight
+
+label <- subset(data, select = c(as.character(update(formula, . ~ .)[[2]])))
+
+independent_vars <- all.vars(formula)[-1]
+features <- data %>% select(independent_vars)
+
+#look for features factor variables 
+factor_variables <- names(features)[sapply(features, is.factor)]
+
+#Create dummy variables from factor variables
+dummy_matrix <- model.matrix(~ . - 1, data = data[, factor_variables])
+features <- cbind(features, dummy_matrix)
+features <- features %>%
+  select(-any_of(factor_variables))
+data_matrix <- xgb.DMatrix(data = as.matrix(features), label = as.matrix(label))
+
+params <- list(
+  objective = "reg:squarederror", 
+  eta = 0.1,                      
+  max_depth = 3
+)
+
+# Perform k-fold cross-validation
+cv_result <- xgb.cv(
+  params = params,
+  data = xgb.DMatrix(data = as.matrix(features), label = as.matrix(label)),
+  nfold = 10,                     # Number of folds
+  verbose = TRUE,
+  nrounds = 15
   
 )
 cv <- data.frame(Boosting_Round =  cv_result$evaluation_log$iter, 
@@ -246,11 +378,83 @@ ggplot(cv, aes(x = Boosting_Round)) +
   theme_minimal() +
   scale_x_continuous(breaks = seq(0, max(cv$Boosting_Round), by = 10))  #
 
+# Estimating
+# Kromat Kylling 2 Enkel uk
+data$treatment <-  ifelse(data$feed_group == "Kromat Kylling 2 Enkel u/k", 1, 0)
+N <- nrow(data) # Bootstrap obs per resampling
+R = 1000 #Number of bootstrap replica
 
+# Creating a matrix of dirichlet weights for Bayesian bootstrap
+dirichlet <- matrix( rexp(N * R, 1) , ncol = N, byrow = TRUE) 
+dirichlet_w <- dirichlet / rowSums(dirichlet)
 
-#-----------------------------------------------------------------------------
-# Direct effect (attempting CATE) where W union X is prod_type, frequent_month, id_slaughterhouse
-#-----------------------------------------------------------------------------
+S_learner_direct <- boot(data=data, 
+                         statistic = S_learner, 
+                         formula = formula, 
+                         weights = dirichlet_w, 
+                         R=rep(1,R),
+                         eta = 0.1,                      
+                         max_depth = 3,
+                         nrounds = 15)
 
+S_CATE_boot_feed1 <- S_learner_direct$t
+save(S_CATE_boot_feed1,file="C:/broiler_acites/ascites_case/Results/S_CATE_boot_feed1.Rda")
+
+# Topp Kylling Netto
+data$treatment <-  ifelse(data$feed_group == "Toppkylling Netto", 1, 0)
+N <- nrow(data) # Bootstrap obs per resampling
+R = 1000 #Number of bootstrap replica
+
+# Creating a matrix of dirichlet weights for Bayesian bootstrap
+dirichlet <- matrix( rexp(N * R, 1) , ncol = N, byrow = TRUE) 
+dirichlet_w <- dirichlet / rowSums(dirichlet)
+S_learner_direct <- boot(data=data, 
+                         statistic = S_learner, 
+                         formula = formula, 
+                         weights = dirichlet_w, 
+                         R=rep(1,R),
+                         eta = 0.1,                      
+                         max_depth = 3,
+                         nrounds = 15)
+
+S_CATE_boot_feed2 <- S_learner_direct$t
+save(S_CATE_boot_feed2,file="C:/broiler_acites/ascites_case/Results/S_CATE_boot_feed2.Rda")
+
+# Topp Kylling Netto
+data$treatment <-  ifelse(data$feed_group == "Kromat Kylling 2 Enkel u/k", 1, 0)
+N <- nrow(data) # Bootstrap obs per resampling
+R = 1000 #Number of bootstrap replica
+
+# Creating a matrix of dirichlet weights for Bayesian bootstrap
+dirichlet <- matrix( rexp(N * R, 1) , ncol = N, byrow = TRUE) 
+dirichlet_w <- dirichlet / rowSums(dirichlet)
+S_learner_direct <- boot(data=data, 
+                         statistic = S_learner, 
+                         formula = formula, 
+                         weights = dirichlet_w, 
+                         R=rep(1,R),
+                         eta = 0.1,                      
+                         max_depth = 3,
+                         nrounds = 15)
+
+S_CATE_boot_feed3 <- S_learner_direct$t
+save(S_CATE_boot_feed3,file="C:/broiler_acites/ascites_case/Results/S_CATE_boot_feed3.Rda")
+
+treat_cate1 <- data.frame(S_CATE_boot_feed1[,1])
+treat_cate2 <- data.frame(S_CATE_boot_feed2[,1])
+treat_cate3 <- data.frame(S_CATE_boot_feed3[,1])
+treat_cate1$feed <- "Feed 1"
+colnames(treat_cate1) <- c('values', 'feed')
+treat_cate2$feed <- "Feed 2"
+colnames(treat_cate2) <- c('values', 'feed')
+treat_cate3$feed <- "Feed 3"
+colnames(treat_cate3) <- c('values', 'feed')
+df <- rbind(treat_cate1, treat_cate2, treat_cate3)
+
+ggplot(data = df, aes(x = feed, y = values)) +
+  geom_boxplot() +
+  xlab("Growth Feed Type") +
+  ylab("Treatment effect on the prevalence (1/1000) ascites") +
+  ggtitle("S-learner ATE Direct") 
 
 

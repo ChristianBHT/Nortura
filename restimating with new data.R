139529861029261c7d1d46a9f2ed96185d86bb40
Christian Thorjussen <christianbern@gmail.com>
2023-10-12 17:35:03 +0200

    d

--- restimating with new data.R
+++ restimating with new data.R
@@ -10,18 +10,17 @@ library(xgboost)
 setwd("C:/Users/christian.thorjussen/Project Nortura/Nytt datauttrekk")
 rm(list = ls())
 
-load("wide_data_for_analysis.Rda")
-wide_data$feed_name <- str_replace(wide_data$feed_name, "�", "aa")
-wide_data$feed_name <- str_replace(wide_data$feed_name, "�", "o")
-wide_data$feed_name <- str_replace(wide_data$feed_name, "o?=", "aa")
+load("wide_newdata_for_analysis.Rda")
+wide_data$feed_name <- str_replace(wide_data$feed, "�", "aa")
+wide_data$feed_name <- str_replace(wide_data$feed, "�", "o")
+wide_data$feed_name <- str_replace(wide_data$feed, "o?=", "aa")
 wide_data <- subset(wide_data, hybrid == "Ross 308")
 data <- wide_data
 
 data$feed_name <- as.factor(data$feed_name)
 data$prod_type <- as.factor(data$prod_type)
 data$id_slaughterhouse <- as.factor(data$id_slaughterhouse)
-data$leverandoer_nr <- data$leverandoer_nr
-data$ascites_prev <- data$ascites/data$n_of_chicken
+data$ascites_prev <- data$ascites/data$N_of_chicken
 
 data$frequent_month <- as.factor(data$frequent_month)
 
